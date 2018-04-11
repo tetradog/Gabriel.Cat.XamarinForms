70a0e75eb1b8a19e4d9d0654c85adff060544704
Gabriel <tetradog@gmail.com>
2018-04-11 16:29:27 +0200

    subir archivos

---
+++ Gabriel.Cat.XamarinForms/Gabriel.Cat.XamarinForms/Controles/LayoutEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Gabriel.Cat.XamarinForms
+{
+    public class LayoutEventArgs<T>:EventArgs where T:View
+    {
+        Layout<T> layout;
+        public LayoutEventArgs(Layout<T> layout)
+        {
+            Layout = layout;
+        }
+        public Layout<T> Layout { get => layout; private set => layout = value; }
+    }
+}
---
+++ Gabriel.Cat.XamarinForms/Gabriel.Cat.XamarinForms/Controles/Menu.cs
@@ -0,0 +1,97 @@
+﻿using AsNum.XFControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Gabriel.Cat.XamarinForms
+{
+    public class Menu : ContentView
+	{
+        bool isMenuVisible;
+        Layout<View> layoutDefault;
+        UniformGrid fields;
+
+        public event EventHandler<LayoutEventArgs<View>> ToSee;
+        public event EventHandler Click;
+		public Menu ()
+		{
+            Button btn = new Button();
+            btn.Clicked += ClickMenuButton;
+            Content = new StackLayout {
+				Children = {
+					btn
+				}
+			};
+            Fields = new UniformGrid();
+            MostrarMenu = false;
+            Fields.ColumnDefinitions.Clear();//asi es una lista de un item por fila o deberia
+            
+		}
+        public bool MostrarMenu {
+            get => isMenuVisible;
+            set {
+                isMenuVisible = value;
+                //cambio imagen
+                PonFondo();
+                //muestro o quito menu
+                if (ToSee != null)
+                {
+                    if (isMenuVisible)
+                    {
+                        ToSee(this, new XamarinForms.LayoutEventArgs<View>(Fields));
+                    }
+                    else
+                    {
+                        ToSee(this, new XamarinForms.LayoutEventArgs<View>(layoutDefault));
+                    }
+                }
+            }
+        }
+
+        public UniformGrid Fields { get => fields; set => fields = value==null?new UniformGrid():value; }
+
+        private void ClickMenuButton(object sender, EventArgs e)
+        {
+            if (Click != null)
+                Click(this, new EventArgs());
+            MostrarMenu=!MostrarMenu;
+
+        }
+
+        public Button AddField(string name,Action action)
+        {
+            Button btn = new Button();
+            btn.Text = name;
+            btn.Clicked += (s, e) => action.Invoke();
+            Fields.Children.Add(btn);
+            return btn;
+        }
+        public Button AddField(string name, Layout<View> toSee)
+        {
+            Button btn = new Button();
+            btn.Text = name;
+            btn.Clicked += (s, e) => { if (ToSee != null) ToSee(this, new LayoutEventArgs<View>(toSee));isMenuVisible = false;PonFondo(); };
+            Fields.Children.Add(btn);
+            return btn;
+        }
+        public void RemoveField(Button field)
+        {
+            Fields.Children.Remove(field);
+        }
+        private void PonFondo()
+        {
+            if (isMenuVisible)
+            {
+                BackgroundColor = Color.LightGray;
+            }
+            else
+            {
+                BackgroundColor = Color.Transparent;
+            }
+        }
+    }
+
+}
