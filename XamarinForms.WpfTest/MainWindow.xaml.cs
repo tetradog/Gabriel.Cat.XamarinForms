c7ea12e4d4017572978d421e513c1db188388c51
Gabriel <tetradog@gmail.com>
2018-04-14 05:05:26 +0200

    desarrollando parte de test ahora en wpf :)

---
+++ XamarinForms.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Windows;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF.Extensions;
+namespace XamarinForms.WpfTest
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            UIElement xfTest;
+            InitializeComponent();
+            Forms.Init();
+            xfTest = new Gabriel.Cat.XamarinForms.PageTest().ToFrameworkElement();
+            gMain.Children.Add(xfTest);
+        }
+    }
+}
